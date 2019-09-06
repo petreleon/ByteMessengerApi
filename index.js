2106bc8b4f20510159468ddb036337ba22bab95f
Petre Leonard Macamete <petreleonardos@gmail.com>
2019-09-06 15:48:44 +0300

    install and use bodyparser

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
+app.use(bodyParser);
 
 app.listen(process.env.PORT, () => console.log(`Rody Messenger listening on port ${process.env.PORT}!`));
