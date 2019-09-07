6d3f5a8ecf965331b5c54d0cc5f413d5bbf52b5e
Petre Leonard Macamete <petreleonardos@gmail.com>
2019-09-08 00:50:58 +0300

    use bcrypt to encrypt passwords

--- api/models/user.js
+++ api/models/user.js
@@ -1,10 +1,33 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
   userName: String,
   password: String,
+});
+
+// eslint-disable-next-line func-names
+userSchema.pre('save', function (next) {
+  const user = this;
+  if (!user.isModified('password')) next();
+  else {
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) next(err);
+
+      else {
+        // eslint-disable-next-line no-shadow
+        bcrypt.hash(user.password, salt, null, (err, hash) => {
+          if (err) next(err);
+          else {
+            user.password = hash;
+            next();
+          }
+        });
+      }
+    });
+  }
 });
 
 const User = mongoose.model('User', userSchema);
