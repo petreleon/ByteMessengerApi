9dae7e92767234cf3027da876770f6a6e684fbdc
Petre Leonard Macamete <petreleonardos@gmail.com>
2019-09-07 05:19:54 +0300

    use mongoose

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import mainRoute from './api/routes/main';
 
 dotenv.config();
+
+// create database connection
+mongoose.connect(process.env.MONGO_COLLECTION);
+mongoose.set('debug', true);
+
 const app = express();
 app.use(bodyParser);
 app.use('/api', mainRoute);
