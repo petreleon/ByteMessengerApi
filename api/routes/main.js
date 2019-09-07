8fafa2f5ba5c09da92cd073fb132719b24864618
Petre Leonard Macamete <petreleonardos@gmail.com>
2019-09-07 04:16:27 +0300

    main route

---
+++ api/routes/main.js
@@ -0,0 +1,5 @@
+import express from 'express';
+
+const mainRoute = express.Router();
+
+export default mainRoute;
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import mainRoute from './api/routes/main'
 
 dotenv.config();
 const app = express();
 app.use(bodyParser);
+app.use('/api', mainRoute);
 
 app.listen(process.env.PORT, () => console.log(`Rody Messenger listening on port ${process.env.PORT}!`));
