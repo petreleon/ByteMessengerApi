da8e611dc560df98d9cefccc2340cc236e1066d3
Petre Leonard Macamete <petreleonardos@gmail.com>
2019-09-08 20:59:37 +0300

    add user route

--- api/routes/main.js
+++ api/routes/main.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import userRoute from './user';
 
 const mainRoute = express.Router();
+mainRoute.use('/users', userRoute);
 
 export default mainRoute;
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,5 +1,27 @@
 import express from 'express';
+import UserController from '../controllers/user';
+import userModel from '../models/user';
+
+const userController = new UserController(userModel);
 
 const userRoute = express.Router();
+
+userRoute.get('/', (req, res) => {
+  console.log('here');
+  userController.getUsers((error, result) => {
+    if (error) return res.status(500).end();
+    return res.json(result).end();
+  });
+});
+
+userRoute.post('/', (req, res) => {
+  userController.addUser(req.body, (err) => {
+    if (err) {
+      return res.status(500).end();
+    }
+    return res.status(201).end();
+  });
+});
+
 
 export default userRoute;
